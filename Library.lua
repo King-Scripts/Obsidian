477ad53b017343217ca0d82a35158eb831213c41
JL <josephleonvivarjeremias@gmail.com>
2026-04-27 02:17:53 -0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -10,6 +10,8 @@ local TextService: TextService = cloneref(game:GetService("TextService"))
 local Teams: Teams = cloneref(game:GetService("Teams"))
 local TweenService: TweenService = cloneref(game:GetService("TweenService"))
 
+local Theme = getgenv().ObsidianTheme
+
 local getgenv = getgenv or function()
     return shared
 end
@@ -219,11 +221,10 @@ local Library = {
 
     IsLightTheme = false,
     Scheme = {
-        BackgroundColor = Color3.fromRGB(15, 15, 15),
-        MainColor = Color3.fromRGB(25, 25, 25),
-        AccentColor = Color3.fromRGB(125, 85, 255),
-        OutlineColor = Color3.fromRGB(40, 40, 40),
-        FontColor = Color3.new(1, 1, 1),
+MainColor = Theme.MainColor
+AccentColor = Theme.AccentColor
+OutlineColor = Theme.OutlineColor
+FontColor = Theme.FontColor
         Font = Font.fromEnum(Enum.Font.Code),
 
         RedColor = Color3.fromRGB(255, 50, 50),
